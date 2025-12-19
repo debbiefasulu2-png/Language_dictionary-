8f597a1d30729efca38e7732e3c5d11874f59a7c
Favejonathan <favourjayy09@gmail.com>
2025-12-19 18:15:15 +0100

    Add files via upload

---
+++ Language_dictionary_(Igbo)/Language_dictionary__Igbo_.py
@@ -0,0 +1,30 @@
+﻿dictionary = {'ndewo': 'hello',
+              'kedu': 'how',
+              'biko': 'please',
+              'daalu': 'thank you',
+              'mba' : 'no',
+              'bia': 'come',
+              'gaa' : 'go',
+              'ụlọ': 'house',
+              'onụ': 'mouth',
+              'anya': 'eye',
+              'azụ': 'back',
+              'aka': 'hand',
+              'nkwụ': 'tree',
+              'ife': 'thing',
+              'mmiri': 'water',
+              'isi': 'head',
+              'nri': 'food',
+              'oche': 'chair',
+              'aha': 'name',
+              'nwa': 'child',}
+
+word = input('Enter a word to translate to english: ').lower()
+if word in dictionary:
+    print('Meaning:', dictionary[word])
+else:
+    print('Soory, that word is not available')
+
+    # Name: Favour Kamsi Jonathan
+    # Matric no : BHU/25/04/05/0001
+    # Department: Computer science
